9c9193c9ed1cd4d1df6db4475586c3774625702f
winks <fa@art-core.org>
2011-07-22 20:49:33 +0200

    Realm stuff semi-working

--- src/battlenet/core.clj
+++ src/battlenet/core.clj
@@ -1,22 +1,76 @@
 (ns battlenet.core
   (:use [clojure.data.json :only (json-str write-json read-json)])
-  (:require clojure.contrib.duck-streams))
+  (:use [clojure.contrib.duck-streams :only (slurp*)])
+  (:import (java.net URL URLConnection HttpURLConnection UnknownHostException)
+           (java.io FileNotFoundException)))
 
-(def baseurl "https://{region}.battle.net/api/{game}{path}?{params}")
+(def bn-baseurl "https://{region}.battle.net/api/{game}{path}?{params}")
+(def bn-path-realm-status "/realm/status")
 
 (defn create-url
-  "builds a request URL"
+  "Builds a request URL."
   [region game path params]
     (.replace
       (.replace
         (.replace
-          (.replace baseurl "{region}" region)
+          (.replace bn-baseurl "{region}" region)
           "{game}" game)
         "{path}" path)
     "{params}" params))
 
 (defn join-params
-  "joins url parameters"
+  "Joins URL parameters."
   [params]
   (str (first params) "=" (apply str (interpose \, (nthnext params 1)))))
 
+(defn check-url
+  "Checks if an URL is readable."
+  [url]
+  (try
+    (let [c (cast HttpURLConnection
+                  (.openConnection (URL. url)))]
+      (if (= 200 (.getResponseCode c))
+        1
+        0))
+    (catch UnknownHostException _
+      0)))
+
+(defn read-url
+  "Reads from an URL."
+  [url]
+  (try
+    (slurp* url)
+    (catch UnknownHostException _
+      0)
+    (catch FileNotFoundException _
+      0)))
+
+(defn get-realm-map
+  "Returns realm info as a map."
+  [region realm]
+  (read-json
+    (read-url
+      (create-url region "wow" bn-path-realm-status (join-params ["realms" realm])))))
+
+(defn realm-is-online
+  "Shows whether a realm is online."
+  [region realm]
+  (let [data (get-realm-map region realm)]
+    (get (first (get data :realms)) :status)))
+
+(defn realm-is-offline
+  "Shows whether a realm is offline."
+  [region realm]
+  (not (realm-is-online region realm)))
+
+(defn realm-has-queue
+  "Shows whether a realm has a queue."
+  [region realm]
+  (let [data (get-realm-map region realm)]
+    (get (first (get data :realms)) :queue)))
+;(println
+;  (has-status
+;    (read-json
+;      (read-url "https://eu.battle.net/api/wow/realm/status?realms=aegwynn"))))
+(println ( realm-is-online "eu" "aegwynn"))
+(println ( realm-is-offline "eu" "aegwynn"))
--- test/battlenet/test/core.clj
+++ test/battlenet/test/core.clj
@@ -2,6 +2,16 @@
   (:use [battlenet.core])
   (:use [clojure.test]))
 
+(def mock-json "{\"realms\":[{
+      \"type\":\"pvp\",
+      \"queue\":false,
+      \"status\":true,
+      \"population\":\"high\",
+      \"name\":\"Aegwynn\",
+      \"slug\":\"aegwynn\"
+    }]}")
+(def mock-map {:realms [{:type "pvp", :queue false, :status true, :population "high", :name "Aegwynn", :slug "aegwynn"}]})
+
 (deftest test-join-params
   (is
     (.equals "realms=aegwynn,aerie-peak"
@@ -11,3 +21,23 @@
   (is
     (.equals "https://eu.battle.net/api/wow/realm/status?realms=aegwynn"
       (create-url "eu" "wow" "/realm/status" "realms=aegwynn"))))
+
+;(deftest test-check-url
+;  (is
+;    (= 1 (read-url "http://www.heise.de/"))))
+
+(deftest test-read-url
+  (is
+    (.startsWith
+      (read-url "https://eu.battle.net/api/wow/realm/status?realms=aegwynn") "{")))
+
+(deftest test-get-realm-map
+  (is
+    (.equals "Aegwynn"
+      (get (first (get mock-map :realms)) :name))))
+
+(deftest test-realm-is-online
+  (is
+    (= true
+      (realm-is-online "eu" "aegwynn"))))
+
