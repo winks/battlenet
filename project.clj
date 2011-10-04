2f4a0c3bf977c8e55fd1b0d0e94ddc5fd1eab16a
Florian Anderiasch <fa@art-core.org>
2011-10-04 13:52:45 +0200

    Trying to package

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject battlenet "0.0.7-SNAPSHOT"
+(defproject battlenet "0.0.8"
   :description "Clojure library for Blizzard's Community Platform API"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/data.json "0.1.1"]]
-  :dev-dependencies [[radagast "1.0.0"]]
+  :dev-dependencies [[radagast "1.0.0"]
+                     [lein-clojars "0.6.0"]]
   :repositories {"clojure-releases" "http://build.clojure.org/releases"}
   :main battlenet.core)
