c55ef68670c38adf44c6175bb13e496d023392d4
Florian Anderiasch <fa@art-core.org>
2025-10-19 11:30:07 +0000

    Bump deps & add missing example config def

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/winks/battlenet"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.clojure/data.json "2.5.0"]
-                 [clj-http "3.13.0"]
-                 [compojure "1.7.1"]
-                 [ring/ring-defaults "0.5.0"]]
-  :plugins [[lein-cloverage "1.2.2"]
-            [lein-ring "0.12.5"]]
+                 [clj-http "3.13.1"]
+                 [compojure "1.7.2"]
+                 [ring/ring-defaults "0.7.0"]]
+  :plugins [[lein-cloverage "1.2.4"]
+            [lein-ring "0.12.6"]]
   :repl-options {:init-ns battlenet.core}
   :main battlenet.core
   :aliases {"cli" ["run" "-m" "battlenet.core"]}
@@ -17,5 +17,5 @@
   :ring {:handler battlenet.handler/app}
   :test-paths ["test/"]
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-mock "0.4.0"]]
+                                  [ring/ring-mock "0.6.2"]]
                    :test-paths ["test/"]}})
